d3af5bc58522408de22818d1d92e3d36d7b3c289
Friendosie <75134000+Friendosie@users.noreply.github.com>
2022-07-04 04:44:27 +0600

    New Usage

--- fcaptcha.py
+++ fcaptcha.py
@@ -1,6 +1,5 @@
 import requests, json
-sis = requests.Session()
-def solve(sid):
+def solve(sid, sis):
 
     r = sis.get(url=f'https://onnxrt.herokuapp.com/solve?sid={sid}').json()
     captxt, capsid = r['text'], r['sid']
